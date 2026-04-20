b1226a4690df4bf73c29e8b09a87fd340b48772c
crssstha <shresthacyrus85@gmail.com>
2026-04-20 16:22:36 +0545

    fix(lint): address the lint not working

--- eslint.config.js
+++ eslint.config.js
@@ -1,143 +1,161 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import airbnb from "eslint-config-airbnb";
-import { defineConfig, globalIgnores } from "eslint/config";
-import importPlugin from 'eslint-plugin-import'
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import importNewlines from "eslint-plugin-import-newlines";
-import reactPlugin from 'eslint-plugin-react'
-import reactCompiler from "eslint-plugin-react-compiler";
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import json from "@eslint/json";
 
+import process from 'process';
 
-export default defineConfig([
-  globalIgnores([
-    "dist",
-    "node_modules/",
-    "build/",
-    "coverage/",
-    "codegen.ts",
-    "generated/types/",
-  ]),
-  {
-    files: ["app/**/*.tsx", "app/**/*.jsx", "app/**/*.ts", "app/**/*.js"],
+const dirname = process.cwd();
+
+const compat = new FlatCompat({
+    baseDirectory: dirname,
+    resolvePluginsRelativeTo: dirname,
+});
+
+const appConfigs = compat.config({
+    env: {
+        node: true,
+        browser: true,
+        es2020: true,
+    },
+    root: true,
     extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
+        'airbnb',
+        'airbnb/hooks',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
     ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        warnOnUnsupportedTypeScriptVersion: false,
-      },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    plugins: {
-      'simple-import-sort': simpleImportSort,
-      'import-newlines': importNewlines,
-      'import': importPlugin,
-      "airbnb": airbnb,
-      "react": reactPlugin,
-      "react-compiler": reactCompiler, 
-    },
+    plugins: [
+        '@typescript-eslint',
+        'react-refresh',
+        'simple-import-sort',
+        'import-newlines'
+    ],
     settings: {
-      "react": {
-        "version": "detect",
-      },
-      "import/parsers": {
-        "@typescript-eslint/parser": [".ts", ".tsx"],
-      },
-      "import/resolver": {
-        typescript: {
-          project: ["./tsconfig.json"],
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx']
         },
-      },
+        'import/resolver': {
+            typescript: {
+                project: [
+                    './tsconfig.json',
+                ],
+            },
+        },
     },
     rules: {
-      "react-refresh/only-export-components": "warn",
+        'react-refresh/only-export-components': 'warn',
 
-      "no-unused-vars": 0,
-      "@typescript-eslint/no-unused-vars": 1,
+        'no-unused-vars': 0,
+        '@typescript-eslint/no-unused-vars': 1,
 
-      "no-use-before-define": 0,
-      "@typescript-eslint/no-use-before-define": 1,
+        'no-use-before-define': 0,
+        '@typescript-eslint/no-use-before-define': 1,
 
-      "no-shadow": 0,
-      "@typescript-eslint/no-shadow": ["error"],
+        'no-shadow': 0,
+        '@typescript-eslint/no-shadow': ['error'],
 
-      "import/no-extraneous-dependencies": [
-        "error",
-        {
-          devDependencies: [
-            "**/*.test.{ts,tsx}",
-            "eslint.config.js",
-            "postcss.config.cjs",
-            "stylelint.config.cjs",
-            "vite.config.ts",
-          ],
-          optionalDependencies: false,
-        },
-      ],
+        'import/no-extraneous-dependencies': [
+            'error',
+            {
+                devDependencies: [
+                    '**/*.test.{ts,tsx}',
+                    'eslint.config.js',
+                    'postcss.config.cjs',
+                    'stylelint.config.cjs',
+                    'vite.config.ts',
+                ],
+                optionalDependencies: false,
+            },
+        ],
 
-      indent: ["error", 4, { SwitchCase: 1 }],
+        indent: ['error', 4, { SwitchCase: 1 }],
 
-      "import/no-cycle": [
-        "error",
-        { allowUnsafeDynamicCyclicDependency: true },
-      ],
+        'import/no-cycle': ['error', { allowUnsafeDynamicCyclicDependency: true }],
 
-      "react/react-in-jsx-scope": "off",
-      camelcase: "off",
+        'react/react-in-jsx-scope': 'off',
+        'camelcase': 'off',
 
-      "react/jsx-indent": ["error", 4],
-      "react/jsx-indent-props": ["error", 4],
-      "react/jsx-filename-extension": [
-        "error",
-        { extensions: [".js", ".jsx", ".ts", ".tsx"] },
-      ],
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+        'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
 
-      "import/extensions": ["off", "never"],
-      "import/named": "warn",
+        'import/extensions': ['off', 'never'],
+        'import/named': 'warn',
 
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
 
-      "react/require-default-props": [
-        "warn",
-        { ignoreFunctionalComponents: true },
-      ],
-      "simple-import-sort/imports": [
-        "error",
-        {
-          groups: [
-            // Side effect imports
-            ["^\\u0000"],
-            // React related packages come first
-            ["^react", "^@?\\w"],
-            // Internal packages
-            ["^#.+$"],
-            // Parent imports, other relative imports
-            [
-              "^\\.\\.(?!/?$)",
-              "^\\.\\./?$",
-              "^\\./(?=.*/)(?!/?$)",
-              "^\\.(?!/?$)",
-              "^\\./?$",
-            ],
-            // Style/asset imports
-            ["^.+\\.json$", "^.+\\.module.css$"],
-          ],
-        },
-      ],
-      "simple-import-sort/exports": "warn",
-      "import-newlines/enforce": ["warn", 1],
+        'react/require-default-props': ['warn', { ignoreFunctionalComponents: true }],
+        'simple-import-sort/imports': 'warn',
+        'simple-import-sort/exports': 'warn',
+        'import-newlines/enforce': ['warn', 1],
 
-      "react/jsx-props-no-spreading": "warn",
+        'react/jsx-props-no-spreading': 'warn'
     },
+    overrides: [
+        {
+            files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+            rules: {
+                'simple-import-sort/imports': [
+                    'error',
+                    {
+                        'groups': [
+                            // side effect imports
+                            ['^\\u0000'],
+                            // packages `react` related packages come first
+                            ['^react', '^@?\\w'],
+                            // internal packages
+                            ['^#.+$'],
+                            // parent imports. Put `..` last
+                            // other relative imports. Put same-folder imports and `.` last
+                            ['^\\.\\.(?!/?$)', '^\\.\\./?$', '^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+                            // style imports
+                            ['^.+\\.json$', '^.+\\.module.css$'],
+                        ]
+                    }
+                ]
+            }
+        }
+    ],
+}).map((conf) => ({
+    ...conf,
+    files: ['app/**/*.tsx', 'app/**/*.jsx', 'app/**/*.ts', 'app/**/*.js'],
+    ignores: [
+        "node_modules/",
+        "build/",
+        "coverage/",
+        "codegen.ts",
+        'generated/types/'
+    ],
 
-  }
-])
+}));
+
+const otherConfig = {
+    files: ['*.js', '*.cjs'],
+    ...js.configs.recommended
+};
+
+const jsonConfig = {
+    files: ['**/*.json'],
+    language: 'json/json',
+    rules: {
+        'json/no-duplicate-keys': 'error',
+    },
+};
+
+
+export default [
+    {
+        plugins: {
+            json,
+        },
+    },
+    ...appConfigs,
+    otherConfig,
+    jsonConfig,
+];
--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,67 @@
-import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths";
-import babel from '@rolldown/plugin-babel'
+import babel from '@rolldown/plugin-babel';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import { execSync } from 'child_process';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+/* Get commit hash */
+function getCommitHash(): string {
+    if (process.env.APP_COMMIT_HASH) {
+        return process.env.APP_COMMIT_HASH;
+    }
+    try {
+        return execSync('git rev-parse --short HEAD').toString().trim();
+    } catch (error) {
+        throw new Error(
+            'Unable to determine commit hash. You must either provide a commit hash using the APP_COMMIT_HASH environment variable,'
+      + " or provide a valid Git repository (submodule doesn't work with docker).",
+        );
+    }
+}
+
+const commitHash = getCommitHash();
 
 export default defineConfig(({ mode }) => {
-  const isProd = mode === "production";
-  return {
-    plugins: [
-        react(),
-        tsconfigPaths(),
-        babel({ presets: [reactCompilerPreset()] })
-      ],
-    css: {
-      devSourcemap: isProd,
-      modules: {
-        scopeBehaviour: "local",
-        localsConvention: "camelCaseOnly",
-      },
-    },
-    sourcemap: isProd,
-    build: {
-      outDir: "build",
-      sourcemap: isProd,
-    },
-    envPrefix: "APP_",
-    server: {
-      port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
-      strictPort: true,
-    },
-  };
+    const isProd = mode === 'production';
+    return {
+        define: {
+            APP_COMMIT_HASH: JSON.stringify(commitHash),
+        },
+        plugins: [
+            isProd
+                ? checker({
+                    typescript: true,
+                    eslint: {
+                        lintCommand: 'eslint ./app',
+                    },
+                    stylelint: {
+                        lintCommand: 'stylelint "./app/**/*.css"',
+                    },
+                })
+                : undefined,
+            svgr(),
+            react(),
+            tsconfigPaths(),
+            babel({ presets: [reactCompilerPreset()] }),
+        ],
+        css: {
+            devSourcemap: isProd,
+            modules: {
+                scopeBehaviour: 'local',
+                localsConvention: 'camelCaseOnly',
+            },
+        },
+        sourcemap: isProd,
+        build: {
+            outDir: 'build',
+            sourcemap: isProd,
+        },
+        envPrefix: 'APP_',
+        server: {
+            port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
+            strictPort: true,
+        },
+    };
 });
